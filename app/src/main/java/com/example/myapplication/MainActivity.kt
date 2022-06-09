c9bd669d86ae91d1d521d61781dca436acbe1a67
MyApplication <sangeethar@bitcot.com>
2022-06-09 15:04:24 +0530

    MyApplication

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun addNickname(view: View)
+    {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val nicknameTextView = findViewById<TextView>(R.id.textView2)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+}
